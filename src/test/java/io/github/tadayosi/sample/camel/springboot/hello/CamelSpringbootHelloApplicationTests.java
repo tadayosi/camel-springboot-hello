14e10fcaa905fc43d5e1bce037fcdd7c22203858
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2021-02-14 19:58:06 +0900

    generated from start.spring.io

---
+++ src/test/java/io/github/tadayosi/sample/camel/springboot/hello/CamelSpringbootHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tadayosi.sample.camel.springboot.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelSpringbootHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
